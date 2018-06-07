70c99e89d1ff1311792e826269187d32d17af7db
Akash Barsagadey <akash20173@gmail.com>
2018-06-07 18:40:19 +0530

    base project

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Contact, ContactService } from './contact.service';
 @Component({
-  moduleId: module.id,
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
